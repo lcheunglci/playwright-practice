648fe684ab953c6fac9a7d95233561487b186469
Lawrence LCI <lcheunglci@gmail.com>
2025-08-30 23:35:50 -0700

    Add test file

---
+++ tests/m3-core-concepts/test-runner.test.ts
@@ -0,0 +1,5 @@
+import test from '@playwright/test';
+
+// note the title must be unique and the file name must contain .test. or .spec.
+test('Test title1', async () => {});
+test('Test title2', async () => {});
