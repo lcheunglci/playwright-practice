bc474d390af3cb514f4f04e0289824ecc0b3d2fc
Lawrence LCI <lcheunglci@gmail.com>
2025-09-06 23:33:50 -0700

    Add click example

--- tests/m5-basic-actions/fill_check.test.ts
+++ tests/m5-basic-actions/fill_check.test.ts
@@ -8,3 +8,28 @@ test('Fill test', async ({ page }) => {
   // await page.getByLabel('Date of birth').fill('10-10-2025'); // bad value
   await page.getByLabel('Date of birth').fill('2025-10-10'); // good value
 });
+
+test('Click demo', async ({ page }) => {
+  await page.goto('/');
+
+  const btn = await page.getByRole('button', { name: 'Register' });
+
+  await btn.click();
+  await btn.click();
+  await btn.click();
+
+  // ...
+
+  for (let i = 0; i < 5; i++) {
+    await btn.click();
+  }
+
+  // most clean
+
+  await btn.click({ clickCount: 5 });
+
+  await btn.click({ button: 'right' }); // modifier using alt, ctrl...
+
+  // double click
+  await btn.dblclick();
+});
