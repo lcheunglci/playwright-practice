ffd105854953b17d8706bd0be3569f436f993382
Lawrence LCI <lcheunglci@gmail.com>
2025-09-12 23:20:39 -0700

    Add example to use custom js to change the local/session storage

--- tests/m6-advanced-actions/browser-storage.test.ts
+++ tests/m6-advanced-actions/browser-storage.test.ts
@@ -26,3 +26,28 @@ test('Local Storage', async ({ page }) => {
   console.log(storage.cookies);
   console.log(storage.origins[0].localStorage); // no way to set or clear the state?
 });
+
+test('Session (or Local) Storage', async ({ page }) => {
+  await page.goto('/');
+
+  const input = page.getByLabel('First name');
+  input.fill(name);
+  await page.getByRole('button', { name: 'Save input' }).click();
+
+  const storage = await page.evaluate(() => window.localStorage); // can replace it with window.sessionStorage
+  console.log(storage);
+
+  // clear
+  await page.evaluate(() => window.localStorage.clear());
+  await page.reload();
+  await expect(input).toHaveValue('');
+
+  // set
+  await page.evaluate(setLocalStorage);
+  await page.reload();
+  await expect(input).toHaveValue('Andrejs');
+});
+
+function setLocalStorage() {
+  localStorage.setItem('firstName', 'Andrejs');
+}
