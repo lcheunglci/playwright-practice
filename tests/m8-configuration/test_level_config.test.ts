a7480a2db3494696eda8f53800805e592418a487
Lawrence LCI <lcheunglci@gmail.com>
2025-09-18 20:59:07 -0700

    Add configuration at test level example

---
+++ tests/m8-configuration/test_level_config.test.ts
@@ -0,0 +1,36 @@
+import test, { chromium } from '@playwright/test';
+
+//test('Playwright: top-level API', async({page, browserName, browser, context}) => {
+test('Playwright: top-level API', async ({ playwright }) => {
+  const firefoxType = await playwright.firefox.launch();
+
+  const ctx = await firefoxType.newContext();
+  const page = await ctx.newPage();
+
+  await page.goto('/');
+
+  const chromium = await playwright.chromium.launch();
+  // ...
+});
+
+test('Config of Browser / Context', async () => {
+  const browser = await chromium.launch({
+    headless: false,
+    slowMo: 2000,
+    downloadsPath: 'your/path',
+  });
+
+  const context = await browser.newContext({
+    baseURL: '',
+    timezoneId: 'America/New_York',
+    locale: 'es-ES',
+    geolocation: { longitude: 12.492507, latitude: 41.8899938 },
+    viewport: { width: 600, height: 400 },
+    javaScriptEnabled: true, // default, can disable
+    acceptDownloads: true, // default, can disable
+  });
+
+  const page = await context.newPage();
+  await page.goto('');
+  await page.getByRole('button', { name: 'Aceptar todo' }).click();
+});
