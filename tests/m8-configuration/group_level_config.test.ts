59870f2d4dc5baa3140d61c17da91c38812f3424
Lawrence LCI <lcheunglci@gmail.com>
2025-09-19 20:54:05 -0700

    Add group configuration test example

---
+++ tests/m8-configuration/group_level_config.test.ts
@@ -0,0 +1,39 @@
+import test, { chromium } from '@playwright/test';
+
+test.use({
+  // geolocation
+  // httpCredentials
+  // viewport
+  // baseURL
+  // javaScriptEnabled
+  // screenshot
+  // video ... // browser.newContext()
+  actionTimeout: 3000,
+  navigationTimeout: 5000,
+  launchOptions: { slowMo: 2000, headless: true }, // browserType.launch()
+  timezoneId: 'America/New_York',
+});
+
+test('Test 1', async ({ page }) => {
+  await page.goto('/');
+  const zone = await getTimeZone(page);
+  console.log(zone);
+});
+
+test.describe('Group Title', () => {
+  test.use({
+    timezoneId: 'America/Toronto', // closer to the test wins
+  });
+
+  test('Test 2', async ({ page }) => {
+    await page.goto('/');
+    const zone = await getTimeZone(page);
+    console.log(zone);
+  });
+});
+
+async function getTimeZone(page) {
+  return await page.evaluate(
+    () => Intl.DateTimeFormat().resolvedOptions().timeZone,
+  );
+}
